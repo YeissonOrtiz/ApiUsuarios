5f22fda58061572f33063fc1b13d09e24f7528d6
YeissonOrtiz <ortizyeison2183@gmail.com>
2022-04-18 21:32:06 -0500

    Deployment to Heroku

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
+
+const cors = require('cors');
+
 const port = process.env.PORT || 3000;
 const routerApi = require('./routes/index');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler');
 
 app.use(express.json())
+
+
+const whitelist = ['http://localhost:8080', 'http://127.0.0.1:5500'];
+const option = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
+app.use(cors(option));
 
 routerApi(app);
 
@@ -14,6 +30,6 @@ app.use(boomErrorHandler);
 app.use(errorHandler);
 
 
-app.get('/', (req, res) => res.json('Hello World!'));
+app.get('/', (req, res) => res.json('ve a /api/users'));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
