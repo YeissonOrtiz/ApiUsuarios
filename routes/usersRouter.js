48003a9ff8a175171d2f38a681e1d6d0514efdb8
YeissonOrtiz <ortizyeison2183@gmail.com>
2022-04-23 15:11:24 -0500

    attemp validate password

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -58,8 +58,9 @@ router.post(
   async (req, res, next) => {
   try {
     const data = req.body;
+    res.status(200).json(data);
     const message =  await controller.login(data);
-    res.status(200).json(message);
+
   } catch (error) {
     next(error);
   }
